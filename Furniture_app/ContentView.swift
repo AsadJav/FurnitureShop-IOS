dc88720dfe774ddd2b26570f2db1996270ca3b50
AsadJav <asadjaved9490@gmail.com>
2023-12-04 17:49:21 +0500

    SwiftUi based Furniture Shop app only UI

---
+++ Furniture_app/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Furniture_app
+//
+//  Created by Abu Anwar MD Abdullah on 14/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            HomeScreenView()
+        }
+        
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+
